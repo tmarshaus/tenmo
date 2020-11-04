9c1977b0f842b70cfa0950055e5d6be527b6544f
Michael Ball <mball567@gmail.com>
2020-11-04 15:59:20 -0500

    Further Functionality updates

--- 18_Capstone/TenmoServer/DAO/TransferDAO.cs
+++ 18_Capstone/TenmoServer/DAO/TransferDAO.cs
@@ -9,7 +9,6 @@ using TenmoServer.Models;
 
 namespace TenmoServer.DAO
 {
-
     public class TransferDAO : ITransferDAO
     {
         private IAccountDAO accountDAO;
@@ -47,19 +46,20 @@ namespace TenmoServer.DAO
             }
         }
 
+        //TODO 01: Set up else for current balance being less than send money balance
         public Account SendMoneyTo(int toUserId, decimal sentMoney) //removed fromUserId
         {
-            //Get current USer id balance 
+            //Get current USer id balance
             int currentUserId = UserService.GetUserId();
             decimal currentUserBalance = accountDAO.GetAccount(currentUserId).Balance;
 
-            //Get toUserid balance 
+            //Get toUserid balance
             decimal toUserBalance = accountDAO.GetAccount(toUserId).Balance;
 
-            //Check balance of current user against sentMoney 
+            //Check balance of current user against sentMoney
             if (currentUserBalance >= sentMoney)
             {
-                //Updated balance for user 
+                //Updated balance for user
                 decimal newCurrentUserBalance = currentUserBalance - sentMoney;
 
                 //Updated balance for toUserId
@@ -70,16 +70,21 @@ namespace TenmoServer.DAO
 
                 //Update balance for toUser
                 UpdateBalance(toUserId, newToUserBalance);
+
+                LogTransfer(currentUserId, toUserId, sentMoney, 2, 2);
+                return accountDAO.GetAccount(currentUserId);
             }
-
-            return accountDAO.GetAccount(currentUserId);
-
+            else
+            {
+                LogTransfer(currentUserId, toUserId, sentMoney, 2, 3);
+                return accountDAO.GetAccount(currentUserId);
+            }
         }
 
-        public bool UpdateBalance (int userId, decimal newBalance)
+        public bool UpdateBalance(int userId, decimal newBalance)
         {
-            string sql = @"Update accounts 
-                               Set balance = @newBalance 
+            string sql = @"Update accounts
+                               Set balance = @newBalance
                                    Where user_id = @userId;";
 
             try
@@ -107,6 +112,82 @@ namespace TenmoServer.DAO
             catch (SqlException ex)
             {
                 Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public bool LogTransfer(int fromId, int toId, decimal sentMoney, int transferType, int transferStatusId)
+        {
+            string sql = @"Insert into transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount)
+                            Values(@transferTypeId, @transferStatusId, @accountFrom, @accountTo, @amount)";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@transferTypeId", transferType);
+                    cmd.Parameters.AddWithValue("@transferStatusId", transferStatusId);
+                    cmd.Parameters.AddWithValue("@accountFrom", fromId);
+                    cmd.Parameters.AddWithValue("@accountTo", toId);
+                    cmd.Parameters.AddWithValue("@amount", sentMoney);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public List<Transfer> GetUserTransfers()
+        {
+            string sql = "Select * from transfers where @currentUserId in (account_from, account_to)";
+            List<Transfer> transfers = new List<Transfer>();
+            int currentUserId = UserService.GetUserId();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@currentUserId", currentUserId);
+
+                    SqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        Transfer tran = new Transfer();
+
+                        tran.TransferId = Convert.ToInt32(rdr["transfer_id"]);
+                        tran.TransferTypeId = Convert.ToInt32(rdr["transfer_type_id"]);
+                        tran.TransferStatusId = Convert.ToInt32(rdr["transfer_status_id"]);
+                        tran.AccountFrom = Convert.ToInt32(rdr["account_from"]);
+                        tran.AccountTo = Convert.ToInt32(rdr["account_to"]);
+                        tran.Amount = Convert.ToDecimal(rdr["amount"]);
+
+                        transfers.Add(tran);
+                    }
+                    return transfers;
+                }
+            }
+            catch (SqlException ex)
+            {
                 throw;
             }
         }
---
+++ 18_Capstone/TenmoServer/Models/Transfer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TenmoServer.Models
+{
+    public class Transfer
+    {
+        public int TransferId { get; set; }
+        public int TransferTypeId { get; set; }
+        public int TransferStatusId { get; set; }
+        public int AccountFrom { get; set; }
+        public int AccountTo { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
