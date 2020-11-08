9c8f8d359130ba652006b7b992a40d609a06d70a
Taylor <tmarshaus@gmail.com>
2020-11-07 19:41:36 -0500

    Updated SendTEBucks safety check and added UI tweaks

--- 18_Capstone/TenmoClient/Views/MainMenu.cs
+++ 18_Capstone/TenmoClient/Views/MainMenu.cs
@@ -28,38 +28,42 @@ namespace TenmoClient.Views
 
         private MenuOptionResult ViewBalance()
         {
-            Console.WriteLine($"Your current account balance is: {apiService.GetAccount().Balance}");
+            Console.WriteLine($"Your current account balance is: {apiService.GetAccount().Balance:C} \n\n\rPress any key to return to the Main Menu");
             return MenuOptionResult.WaitAfterMenuSelection;
         }
 
         private MenuOptionResult ViewTransfers()
         {
             List<Transfer> userTransfers = apiService.GetUserTransfers();
-
+            Console.WriteLine("Transfer History");
+            Console.WriteLine("-------------------------------------------");
             foreach (Transfer tran in userTransfers)
             {
                 if (tran.AccountFrom == UserService.GetUserId())
                 {
-                    Console.WriteLine($"Id:{tran.TransferId}         To:{tran.UsernameTo}         Amount:{tran.Amount}");
+                    Console.WriteLine($"Transfer ID:{tran.TransferId}\tTo:{tran.UsernameTo}\tAmount:{tran.Amount:C}");
                 }
                 else
                 {
-                    Console.WriteLine($"Id:{tran.TransferId}         From:{tran.UsernameFrom}         Amount:{tran.Amount}");
+                    Console.WriteLine($"Transfer ID:{tran.TransferId}\tFrom:{tran.UsernameFrom}\tAmount:{tran.Amount:C}");
                 }
             }
-
-            Console.WriteLine("Please enter transfer ID to view more details: ");
+            Console.WriteLine("-------------------------------------------");
+            Console.Write("Please enter Transfer ID to view more details: ");
 
             Int32.TryParse(Console.ReadLine(), out int toTransferId);
             TransferDetails details = apiService.GetTransferDetails(toTransferId);
-
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Transfer Details");
+            Console.WriteLine("-------------------------------------------\n");
             Console.WriteLine($"Id: {details.Id}");
             Console.WriteLine($"Account from: {details.From}");
             Console.WriteLine($"Account to: {details.To}");
             Console.WriteLine($"Type: {details.Type}");
             Console.WriteLine($"Status: {details.Status}");
-            Console.WriteLine($"Amount: {details.Amount}");
-
+            Console.WriteLine($"Amount: {details.Amount:C}\n");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Please press any key to return to the Main Menu");
             return MenuOptionResult.WaitAfterMenuSelection;
         }
 
@@ -73,27 +77,45 @@ namespace TenmoClient.Views
         {
             //Get list of all users and return them
             List<User> users = apiService.GetUsers();
+
+            Console.WriteLine("Send TE Bucks");
+            Console.WriteLine("-------------------------------------------");
             foreach (User user in users)
             {
                 if (user.UserId != UserService.GetUserId())
                 {
-                    Console.WriteLine($"User ID: {user.UserId}     Username: {user.Username}");
+                    Console.WriteLine($"User ID: {user.UserId} \t Username: {user.Username}");
                 }
             }
+            Console.WriteLine("-------------------------------------------");
+
             //Ask for user input of ID
-            Console.WriteLine("Please input the User ID you would like to send TE Bucks: ");
+            Console.Write("Please input the User ID for whom you are sending TE Bucks: ");
 
             //Read user input
             Int32.TryParse(Console.ReadLine(), out int toUserId);
 
-            //Ask for user input of money needing to be sent
-            Console.WriteLine("How much money would youlike to send?");
+            //Ask for user input of amount to send
+            Console.Write("Please input the amount of money you are sending: ");
 
             //Read input
             Decimal.TryParse(Console.ReadLine(), out decimal moneySent);
 
-            //Use SendMoney method
-            apiService.SendMoney(toUserId, moneySent);
+            Console.WriteLine("-------------------------------------------");
+
+            //Check balance to determine whether or not transaction is approved 
+            if (apiService.GetAccount().Balance >= moneySent)
+            {
+                //Use SendMoney method
+                apiService.SendMoney(toUserId, moneySent);
+                Console.WriteLine("Send TE Bucks transaction is approved! \n");
+            }
+            else
+            {
+                Console.WriteLine("Send TE Bucks Transaction Denied: Insufficient Funds \n");
+            }
+
+            Console.WriteLine("Press any key to return to main menu");
 
             return MenuOptionResult.WaitAfterMenuSelection;
         }
